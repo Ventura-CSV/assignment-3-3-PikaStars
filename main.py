ad5f3cdb93de2b6ffd2f180febe2f48bc52c5951
PikaStars <pikachustarsza@gmail.com>
2024-06-26 21:44:32 -0700

    commit2

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def main():
+    
     email = input('Enter your email: ')
     """
     ########################################
     Code Your Program here
     ########################################
     """
-    result = True
-    print("9".isalpha())
+    result = False
+    
+    if (email[0].isalpha()):
+        result = True
+    print(result)
     ########################################
     # Do not delete the return statement
     ########################################
